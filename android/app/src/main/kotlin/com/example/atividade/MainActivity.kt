b447ba5d886a4ed6bf02bbaa167938936211b582
Quévin <thexof02@gmail.com>
2024-09-11 19:32:10 -0300

    Avaliação

---
+++ android/app/src/main/kotlin/com/example/atividade/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atividade
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tema = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home - 2H3"),
+        backgroundColor: tema.colorScheme.inversePrimary,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Quévin Willian da Silva"),
+            Text("Atividade Avaliativa"),
+            Text("2024"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Aplicacao extends StatelessWidget {
+  const Aplicacao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "MeuApp",
+      home: HomePage(),
+    );
+  }
+}
+
+void main() => runApp(const Aplicacao());
